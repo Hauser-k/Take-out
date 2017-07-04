a1f32068fe717d942e1a18807de45592205799a1
ling769670942 <769670942@qq.com>
2017-07-04 17:04:37 +0800

    0704第二次提交

--- app/Http/Controllers/Seller/LoginController.php
+++ app/Http/Controllers/Seller/LoginController.php
@@ -8,6 +8,10 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 require_once app_path().'/Org/code/Code.class.php';
 use App\Org\code\Code;
+use Input;
+use Validator;
+use App\Http\Model\Seller\User;
+use Illuminate\Support\Facades\Crypt;
 
 class LoginController extends Controller
 {
@@ -30,6 +34,34 @@ class LoginController extends Controller
     {
         $code = new Code();
         $code->make();
+    }
+
+    public function postDologin(Request $request)
+    {
+       $input = Input::except('_token');
+       
+       $roles = [
+            'sname' => 'required|unique:posts|between:6,12',
+            'spwd' => 'required|between:6,12',
+            'code' => 'required'
+       ];
+        $validator = Validator::make($input,$roles);
+
+        if ($validator->fails()) {
+            return back()->with('error');
+        }else{
+            //验证验证码
+            if(strtoupper($input['code']) != $session('code')){
+                return back() -> with('error','验证码错误');
+            }
+            //验证是否有用户
+            $user = User::where('sname',$input['sname']) -> first();
+            if(!$user){
+                echo 1;
+            }
+        }
+        
+        
     }
   
 }
--- resources/views/seller/login.blade.php
+++ resources/views/seller/login.blade.php
@@ -10,18 +10,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="renderer" content="webkit">
     <meta http-equiv="Cache-Control" content="no-siteapp" />
-    <link rel="icon" type="image/png" href="{{url('assets/i/favicon.png')}}">
-    <link rel="apple-touch-icon-precomposed" href="{{url('assets/i/app-icon72x72@2x.png')}}">
+    <link rel="icon" type="image/png" href="{{asset('assets/i/favicon.png')}}">
+    <link rel="apple-touch-icon-precomposed" href="{{asset('assets/i/app-icon72x72@2x.png')}}">
     <!-- <meta name="apple-mobile-web-app-title" content="Amaze UI" /> -->
-    <link rel="stylesheet" href="{{url('assets/css/amazeui.min.css')}}" />
-    <link rel="stylesheet" href="{{url('assets/css/amazeui.datatables.min.css')}}" />
-    <link rel="stylesheet" href="{{url('assets/css/app.css')}}">
-    <script src="{{url('assets/js/jquery.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('assets/css/amazeui.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/amazeui.datatables.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/app.css')}}">
+    <script src="{{asset('assets/js/jquery.min.js')}}"></script>
 
 </head>
 
 <body data-type="login">
-    <script src="{{url('assets/js/theme.js')}}"></script>
+    <script src="{{asset('assets/js/theme.js')}}"></script>
     <div class="am-g tpl-g">
         <!-- 风格切换 -->
         <div class="tpl-skiner">
@@ -40,24 +40,25 @@
         <div class="tpl-login">
             <div class="tpl-login-content">
                 <div class="">
-                    <img src="{{url('assets/img/logoa.png')}}" alt="">
+                    <img src="{{asset('assets/img/logoa.png')}}" alt="">
                 </div>
-                <form class="am-form tpl-form-line-form">
+                <form class="am-form tpl-form-line-form" method="post" action="{{url('/seller/login/dologin')}}">
+                {{csrf_field()}}
                     <div class="am-form-group">
-                        <input type="text" class="tpl-form-input" id="user-name" placeholder="请输入6-12位账号"><span></span>
+                        <input type="text" class="tpl-form-input" id="user-name" name="sname" placeholder="请输入6-12位账号"><span class="zhanghao"></span>
 
                     </div>
 
                     <div class="am-form-group">
-                        <input type="password" class="tpl-form-input" id="user-name" placeholder="请输入密码">
+                        <input type="password" class="tpl-form-input" name="spwd" id="user-name" placeholder="请输入密码"><span class="mima"></span>
 
                     </div>
                     <div>
                     <input type="text" class="code" name="code" placeholder="请输入验证码" width="20px" /><br>
-                        <span><i class="fa fa-check-square-o"></i></span>
-                        <img src="{{url('seller/code')}}" alt="" onclick="this.src='{{url('seller/code')}}?'+Math.random()">
+                        <span><i class="fa fa-check-square-o"></i></span><span class="yanzhengma"></span>
+                        <img src="{{asset('seller/code')}}" alt="" onclick="this.src='{{asset('seller/code')}}?'+Math.random()">
                         {{--<a onclick="javascript:re_captcha();">--}}
-                            {{--<img src="{{ URL('/code/captcha/1') }}" id="127ddf0de5a04167a9e427d883690ff6">--}}
+                            {{--<img src="{{ asset('/code/captcha/1') }}" id="127ddf0de5a04167a9e427d883690ff6">--}}
                         {{--</a>--}}
                     </div><br>  
                     <div class="am-form-group tpl-login-remember-me">
@@ -68,25 +69,24 @@
                     </div>
                     <div class="am-form-group">
 
-                        <button type="button" class="am-btn am-btn-primary  am-btn-block tpl-btn-bg-color-success  tpl-login-btn">提交</button>
+                        <button type="submit" class="am-btn am-btn-primary  am-btn-block tpl-btn-bg-color-success  tpl-login-btn" id="tijiao">提交</button>
 
                     </div>
                 </form>
             </div>
         </div>
     </div>
-    <script src="{{url('assets/js/amazeui.min.js')}}"></script>
-    <script src="{{url('assets/js/app.js')}}"></script>
-    <script src="{{url('assets/js/denglutijiao.js')}}"></script>
+    <script src="{{asset('assets/js/amazeui.min.js')}}"></script>
+    <script src="{{asset('assets/js/app.js')}}"></script>
+    <script src="{{asset('assets/js/denglutijiao.js')}}"></script>
     <script type="text/javascript">
         function re_captcha() {
-            $url = "{{ URL('/code/captcha') }}";
-            $url = $url + "/" + Math.random();
-            document.getElementById('127ddf0de5a04167a9e427d883690ff6').src = $url;
+            $asset = "{{ asset('/code/captcha') }}";
+            $asset = $asset + "/" + Math.random();
+            document.getElementById('127ddf0de5a04167a9e427d883690ff6').src = $asset;
         }
     </script>
-
-
+   
 </body>
 
 </html>
